c3976837d08766bf47fa475e9f10240cbbfd6f43
pinheirodps <pinheirodps@gmail.com>
2018-06-20 23:12:54 +0100

    Entities gererated

---
+++ src/main/webapp/app/entities/blog/blog.module.ts
@@ -0,0 +1,51 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { JhipsterSampleApplicationSharedModule } from '../../shared';
+import { JhipsterSampleApplicationAdminModule } from '../../admin/admin.module';
+import {
+    BlogService,
+    BlogPopupService,
+    BlogComponent,
+    BlogDetailComponent,
+    BlogDialogComponent,
+    BlogPopupComponent,
+    BlogDeletePopupComponent,
+    BlogDeleteDialogComponent,
+    blogRoute,
+    blogPopupRoute,
+} from './';
+
+const ENTITY_STATES = [
+    ...blogRoute,
+    ...blogPopupRoute,
+];
+
+@NgModule({
+    imports: [
+        JhipsterSampleApplicationSharedModule,
+        JhipsterSampleApplicationAdminModule,
+        RouterModule.forChild(ENTITY_STATES)
+    ],
+    declarations: [
+        BlogComponent,
+        BlogDetailComponent,
+        BlogDialogComponent,
+        BlogDeleteDialogComponent,
+        BlogPopupComponent,
+        BlogDeletePopupComponent,
+    ],
+    entryComponents: [
+        BlogComponent,
+        BlogDialogComponent,
+        BlogPopupComponent,
+        BlogDeleteDialogComponent,
+        BlogDeletePopupComponent,
+    ],
+    providers: [
+        BlogService,
+        BlogPopupService,
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class JhipsterSampleApplicationBlogModule {}
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JhipsterSampleApplicationBlogModule } from './blog/blog.module';
+import { JhipsterSampleApplicationEntryModule } from './entry/entry.module';
+import {JhipsterSampleApplicationTagModule} from "./tag/tag.module";
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        JhipsterSampleApplicationBlogModule,
+        JhipsterSampleApplicationEntryModule,
+        JhipsterSampleApplicationTagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
